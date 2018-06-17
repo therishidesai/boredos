73d1d0518c9507e25e13dc9ce940341300ca563e
Rishi Desai <desai.rishi1@gmail.com>
2018-06-16 21:33:26 -0700

    working kernel hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(panic_implementation)]
+#![feature(core_intrinsics)]
+#![no_std]
+#![no_main]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_implementation]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+    unsafe { intrinsics::abort() }
+}
+
+#[no_mangle]
+pub fn _start() -> ! {
+    loop {}
 }
